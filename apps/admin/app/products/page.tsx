d03cdd07dc77d1035c49c1a2d5c69b389fb77f3f
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2024-12-30 23:04:13 +0200

    create a data table

--- apps/admin/app/products/page.tsx
+++ apps/admin/app/products/page.tsx
@@ -1,7 +1,13 @@
+import { columns } from "@/components/products/columns";
+import { DataTable } from "@/components/products/data-table";
+
 export default function ProductsPage() {
   return (
     <div>
       <h1>Products</h1>
+      <div className="mt-10">
+        <DataTable columns={columns} />
+      </div>
     </div>
   );
 }
