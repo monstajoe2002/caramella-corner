09665c380bfca0578ee6ead0590022905049d4f1
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2024-12-30 23:09:55 +0200

    fill the data table with random values

--- apps/admin/app/products/page.tsx
+++ apps/admin/app/products/page.tsx
@@ -1,12 +1,20 @@
 import { columns } from "@/components/products/columns";
 import { DataTable } from "@/components/products/data-table";
-
+import { faker } from "@faker-js/faker";
 export default function ProductsPage() {
+  const data = new Array(5).fill(null).map(() => ({
+    name: faker.commerce.product(),
+    quantity: faker.number.int({
+      min: 1,
+      max: 10,
+    }),
+    active: faker.datatype.boolean(),
+  }));
   return (
     <div>
       <h1>Products</h1>
       <div className="mt-10">
-        <DataTable columns={columns} />
+        <DataTable columns={columns} data={data} />
       </div>
     </div>
   );
