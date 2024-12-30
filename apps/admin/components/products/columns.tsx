ba960d4896cc3156c845b22209a438db2637519e
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2024-12-30 22:57:06 +0200

    define columns for the products table

---
+++ apps/admin/components/products/columns.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+// TODO: define the type for the data
+
+export const columns: ColumnDef<{
+  name: string;
+  quantity: number;
+  active: boolean;
+}>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "quantity",
+    header: "Quantity",
+  },
+  {
+    accessorKey: "active",
+    header: "Active",
+  },
+];
