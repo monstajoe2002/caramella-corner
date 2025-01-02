35012f38a8abd90dad27e89aa5630127e7a2395f
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2025-01-02 21:57:14 +0200

    create a product form component

--- apps/admin/app/products/new/page.tsx
+++ apps/admin/app/products/new/page.tsx
@@ -1,3 +1,10 @@
+import ProductForm from "@/components/products/product-form";
+
 export default function NewProductPage() {
-  return <div>Create a new product</div>;
+  return (
+    <div>
+      <h1>Create a new product</h1>
+      <ProductForm />
+    </div>
+  );
 }
---
+++ apps/admin/components/products/product-form.tsx
@@ -0,0 +1,209 @@
+"use client";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { cn } from "@caramella-corner/ui/lib/utils";
+import { Button } from "@caramella-corner/ui/components/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@caramella-corner/ui/components/form";
+import { Input } from "@caramella-corner/ui/components/input";
+import { Textarea } from "@caramella-corner/ui/components/textarea";
+import LocationSelector from "@caramella-corner/ui/components/location-input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@caramella-corner/ui/components/select";
+import { Switch } from "@caramella-corner/ui/components/switch";
+
+const formSchema = z.object({
+  name: z.string(),
+  description: z.string(),
+  material: z.string(),
+  countryOfOrigin: z.tuple([z.string(), z.string().optional()]),
+  image: z.string(),
+  subcategory: z.string(),
+  active: z.boolean(),
+});
+
+export default function ProductForm() {
+  const [countryName, setCountryName] = useState<string>("");
+  const [stateName, setStateName] = useState<string>("");
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      console.log(values);
+    } catch (error) {
+      console.error("Form submission error", error);
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 max-w-3xl mx-auto py-10"
+      >
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Blue Blouse" type="" {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Flattering, relaxed fit that effortlessly transitions from work to weekend"
+                  className="resize-none"
+                  {...field}
+                />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="material"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Material</FormLabel>
+              <FormControl>
+                <Input placeholder="100% cotton" type="" {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="countryOfOrigin"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Country of Origin</FormLabel>
+              <FormControl>
+                <LocationSelector
+                  onCountryChange={(country) => {
+                    setCountryName(country?.name || "");
+                    form.setValue(field.name, [
+                      country?.name || "",
+                      stateName || "",
+                    ]);
+                  }}
+                  onStateChange={(state) => {
+                    setStateName(state?.name || "");
+                    form.setValue(field.name, [
+                      form.getValues(field.name)[0] || "",
+                      state?.name || "",
+                    ]);
+                  }}
+                />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="image"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Image</FormLabel>
+              <FormControl>
+                <Input placeholder="" type="file" {...field} />
+              </FormControl>
+              <FormDescription>
+                Select the image that best represents your product.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="subcategory"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Category</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select subcategory" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="m@example.com">m@example.com</SelectItem>
+                  <SelectItem value="m@google.com">m@google.com</SelectItem>
+                  <SelectItem value="m@support.com">m@support.com</SelectItem>
+                </SelectContent>
+              </Select>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="active"
+          render={({ field }) => (
+            <FormItem className="flex flex-row items-center justify-between rounded-lg border p-4">
+              <div className="space-y-0.5">
+                <FormLabel>Mark as Active</FormLabel>
+                <FormDescription>
+                  This product will appear on the storefront, not kept as a
+                  draft.
+                </FormDescription>
+              </div>
+              <FormControl>
+                <Switch
+                  checked={field.value}
+                  onCheckedChange={field.onChange}
+                  aria-readonly
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full">
+          Submit
+        </Button>
+      </form>
+    </Form>
+  );
+}
