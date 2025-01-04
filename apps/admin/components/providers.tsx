162381c020528f985f920de5ba18f69dace040a8
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2025-01-04 16:52:45 +0200

    setup react query

--- apps/admin/components/providers.tsx
+++ apps/admin/components/providers.tsx
@@ -4,15 +4,27 @@ import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { SidebarProvider } from "@caramella-corner/ui/components/sidebar";
 
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+
+const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <NextThemesProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <SidebarProvider>{children}</SidebarProvider>
-    </NextThemesProvider>
+    <ReactQueryProvider>
+      <NextThemesProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <SidebarProvider>{children}</SidebarProvider>
+      </NextThemesProvider>
+    </ReactQueryProvider>
   );
 }
