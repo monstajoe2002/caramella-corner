7ffcb681bb2a78ff60348505fe578b23566a90f1
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2025-01-06 13:54:36 +0200

    add loading state to products page with skeleton component

--- apps/admin/app/products/page.tsx
+++ apps/admin/app/products/page.tsx
@@ -3,9 +3,10 @@ import { columns } from "@/components/products/columns";
 import { DataTable } from "@/components/data-table";
 import { useQuery } from "@tanstack/react-query";
 import { Product } from "@caramella-corner/database/lib/types";
+import { Skeleton } from "@caramella-corner/ui/components/skeleton";
 
 export default function ProductsPage() {
-  const { data } = useQuery<Product[]>({
+  const { data, isLoading } = useQuery<Product[]>({
     queryKey: ["products"],
     queryFn: async () => {
       const res = await fetch("/api/products");
@@ -16,14 +17,18 @@ export default function ProductsPage() {
     <div>
       <h1>Products</h1>
       <div className="mt-10">
-        <DataTable
-          columns={columns}
-          data={data ?? []}
-          showAddButton
-          addButtonLabel="Add Product"
-          addButtonType="link"
-          addButtonHref="/products/new"
-        />
+        {isLoading ? (
+          <Skeleton className="h-[275px]" />
+        ) : (
+          <DataTable
+            columns={columns}
+            data={data ?? []}
+            showAddButton
+            addButtonLabel="Add Product"
+            addButtonType="link"
+            addButtonHref="/products/new"
+          />
+        )}
       </div>
     </div>
   );
