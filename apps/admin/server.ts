43e672ca47c0e225daaeed0967e3c4519f873b15
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2025-01-06 14:07:24 +0200

    delete server.ts

--- apps/admin/server.ts
+++
@@ -1,6 +0,0 @@
-import { connectToDatabase } from "@caramella-corner/database/lib/connection";
-
-// eslint-disable-next-line turbo/no-undeclared-env-vars
-const MONGODB_URI = process.env.MONGO_URI!;
-
-await connectToDatabase(MONGODB_URI);
