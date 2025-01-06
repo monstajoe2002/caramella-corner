57de16ce5dd81bf9a2a6c96bc874dc46fb7df61b
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2025-01-06 15:17:44 +0200

    feat: add getProductById function to retrieve product by ID

--- packages/database/src/admin/products.ts
+++ packages/database/src/admin/products.ts
@@ -8,3 +8,14 @@ export const getProducts = async () => {
     throw error;
   }
 };
+export const getProductById = async (productId: string) => {
+  try {
+    const product = await ProductModel.findById(productId).lean();
+    if (!product) {
+      throw new Error("Product not found");
+    }
+    return product;
+  } catch (error) {
+    throw error;
+  }
+};
