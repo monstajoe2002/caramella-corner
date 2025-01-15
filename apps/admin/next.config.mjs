87fa804ac8be3b1082368de344f36682644ac3e2
monstajoe2002 <youssef.amr90@gmail.com>
2025-01-15 18:44:52 +0200

    feat: add mongoose to server external packages in next.config

--- apps/admin/next.config.mjs
+++ apps/admin/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@caramella-corner/ui"],
+  serverExternalPackages: ["mongoose"],
 };
 
 export default nextConfig;
