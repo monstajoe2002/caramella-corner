6d020721b6fe9be63531954f3ed397dd94944d8e
monstajoe2002 <youssef.amr90@gmail.com>
2025-01-16 02:12:18 +0200

    refactor: replace direct product model usage with createProduct function in new route

--- apps/admin/app/api/products/new/route.ts
+++ apps/admin/app/api/products/new/route.ts
@@ -1,9 +1,9 @@
-import { ProductModel } from "@caramella-corner/database/models/product";
+import { createProduct } from "@caramella-corner/database/admin/products";
 
 export async function POST(request: Request) {
   try {
     const body = await request.json();
-    const product = await ProductModel.create(body);
+    const product = await createProduct(body);
     return new Response(JSON.stringify(product), { status: 200 });
   } catch (error) {
     return new Response(JSON.stringify(error), { status: 500 });
