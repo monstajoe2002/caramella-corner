299e8c08de43815b4ff6397725825b27595d1778
monstajoe2002 <youssef.amr90@gmail.com>
2025-01-19 19:11:44 +0200

    feat: remove fake data from CategoriesPage and update DataTable to use an empty array

--- apps/admin/app/categories/page.tsx
+++ apps/admin/app/categories/page.tsx
@@ -1,25 +1,14 @@
 import { columns } from "@/components/categories/columns";
 import { DataTable } from "@/components/data-table";
-import { faker } from "@faker-js/faker";
 
 export default function CategoriesPage() {
-  const data = [
-    {
-      name: faker.commerce.department(),
-      subcategories: faker.helpers.uniqueArray(faker.commerce.department, 5),
-    },
-    {
-      name: faker.commerce.department(),
-      subcategories: faker.helpers.uniqueArray(faker.commerce.department, 5),
-    },
-  ];
   return (
     <div>
       <h1>Categories</h1>
       <div className="mt-10">
         <DataTable
           columns={columns}
-          data={data}
+          data={[]}
           addButtonLabel="Create Category"
           showAddButton
           addButtonType="dialog"
