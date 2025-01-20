eafe3d4631cc6984a618d93ecf769d0916da970d
monstajoe2002 <youssef.amr90@gmail.com>
2025-01-20 21:30:57 +0200

    display categories data

--- apps/admin/app/categories/page.tsx
+++ apps/admin/app/categories/page.tsx
@@ -1,15 +1,25 @@
+"use client";
 import CategoryForm from "@/components/categories/category-form";
 import { columns } from "@/components/categories/columns";
 import { DataTable } from "@/components/data-table";
+import { Category } from "@caramella-corner/database/types/index";
+import { useQuery } from "@tanstack/react-query";
 
 export default function CategoriesPage() {
+  const { data, isLoading } = useQuery<Category[]>({
+    queryKey: ["categories"],
+    queryFn: async () => {
+      const res = await fetch("/api/categories");
+      return res.json();
+    },
+  });
   return (
     <div>
       <h1>Categories</h1>
       <div className="mt-10">
         <DataTable
           columns={columns}
-          data={[]}
+          data={data ?? []}
           addButtonLabel="Create Category"
           showAddButton
           addButtonType="dialog"
