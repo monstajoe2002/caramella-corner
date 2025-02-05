e2d09f7e3f585c7bd9259aee6dd6956f4c31c12e
monstajoe2002 <youssef.amr90@gmail.com>
2025-02-05 20:05:29 +0200

    feat: add getTotalRevenue function to calculate total revenue from orders

--- packages/database/src/admin/orders.ts
+++ packages/database/src/admin/orders.ts
@@ -1,7 +1,15 @@
 import { OrderModel } from "../models/order";
+import { Order } from "../types";
 
 export const getOrders = async () => {
   const ordersData = await OrderModel.find().lean();
   const orderCount = await OrderModel.countDocuments();
   return { orders: ordersData || [], count: orderCount || 0 };
+};
+
+export const getTotalRevenue = async () => {
+  const [totalRevenue] = await OrderModel.aggregate([
+    { $group: { _id: null, revenue: { $sum: "$pricePaidInPiasters" } } },
+  ]);
+  return totalRevenue;
 };
