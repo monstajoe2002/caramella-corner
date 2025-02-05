40825334369b68352f198ed775ec10745b662f0b
monstajoe2002 <youssef.amr90@gmail.com>
2025-02-05 20:10:34 +0200

    feat: add getMonthlyCustomers function to retrieve monthly customer counts

---
+++ packages/database/src/admin/customers.ts
@@ -0,0 +1,13 @@
+import { CustomerModel } from "../models/customer";
+
+export const getMonthlyCustomers = async () => {
+  const customerCount = await CustomerModel.aggregate([
+    {
+      $group: {
+        _id: { month: "$createdAt.getMonth()" },
+        count: { $sum: 1 },
+      },
+    },
+  ]);
+  return customerCount || 0;
+};
