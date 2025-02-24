aa21b7a9c98fdf7dc6b72d9f7739d6fa294c74c8
monstajoe2002 <youssef.amr90@gmail.com>
2025-02-24 12:07:54 +0200

    feature: display variants outside dialog on the form

--- apps/admin/components/products/variant-dialog.tsx
+++ apps/admin/components/products/variant-dialog.tsx
@@ -15,89 +15,99 @@ import { useFieldArray, useFormContext } from "react-hook-form";
 import { VariantFormField } from "@/components/products/variant-form-field";
 
 export const VariantDialog = () => {
-  const { control } = useFormContext();
+  const { control, getValues } = useFormContext();
   const { fields, append, remove } = useFieldArray({
     control,
     name: "variants",
   });
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button type="button" className="w-fit">
-          <ChartNoAxesGantt />
-          <span>Manage Variants</span>
-        </Button>
-      </DialogTrigger>
-      <DialogContent className="max-w-[800px]">
-        <DialogHeader>
-          <DialogTitle>Manage Variants</DialogTitle>
-          <DialogDescription>
-            If your product has multiple variants, you can manage them here. You
-            can add different options, e.g. color and size.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="space-y-6">
-          {fields.map((field, index) => (
-            <div key={field.id} className="relative p-4 border rounded-lg">
-              <h3 className="font-medium mb-4">Variant #{index + 1}</h3>
-
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <VariantFormField
-                  index={index}
-                  name="sku"
-                  label="SKU"
-                  placeholder="CC001"
-                />
-                <VariantFormField
-                  index={index}
-                  name="quantity"
-                  label="Quantity"
-                  type="number"
-                  placeholder="10"
-                />
-                <VariantFormField
-                  index={index}
-                  name="color"
-                  label="Color"
-                  placeholder="Pink"
-                />
-                <VariantFormField
-                  index={index}
-                  name="size"
-                  label="Size"
-                  placeholder="L / 42"
-                />
-              </div>
-
-              <Button
-                type="button"
-                variant="destructive"
-                size="sm"
-                className="mt-4"
-                onClick={() => remove(index)}
-              >
-                Remove Variant
-              </Button>
-              <Separator className="my-4" />
-            </div>
-          ))}
-
-          <Button
-            type="button"
-            onClick={() =>
-              append({
-                sku: "",
-                quantity: 0,
-                color: "",
-                size: "",
-              })
-            }
-            className="w-full"
-          >
-            Add New Variant
-          </Button>
+    <div>
+      {fields.map((field, index) => (
+        <div key={field.id}>
+          <p>{getValues(`variants.${index}.sku`)}</p>
+          <p>{getValues(`variants.${index}.quantity`)}</p>
+          <p>{getValues(`variants.${index}.color`)}</p>
+          <p>{getValues(`variants.${index}.size`)}</p>
         </div>
-      </DialogContent>
-    </Dialog>
+      ))}
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button type="button" className="w-fit">
+            <ChartNoAxesGantt />
+            <span>Manage Variants</span>
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="max-w-[800px]">
+          <DialogHeader>
+            <DialogTitle>Manage Variants</DialogTitle>
+            <DialogDescription>
+              If your product has multiple variants, you can manage them here.
+              You can add different options, e.g. color and size.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="space-y-6">
+            {fields.map((field, index) => (
+              <div key={field.id} className="relative p-4 border rounded-lg">
+                <h3 className="font-medium mb-4">Variant #{index + 1}</h3>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                  <VariantFormField
+                    index={index}
+                    name="sku"
+                    label="SKU"
+                    placeholder="CC001"
+                  />
+                  <VariantFormField
+                    index={index}
+                    name="quantity"
+                    label="Quantity"
+                    type="number"
+                    placeholder="10"
+                  />
+                  <VariantFormField
+                    index={index}
+                    name="color"
+                    label="Color"
+                    placeholder="Pink"
+                  />
+                  <VariantFormField
+                    index={index}
+                    name="size"
+                    label="Size"
+                    placeholder="L / 42"
+                  />
+                </div>
+
+                <Button
+                  type="button"
+                  variant="destructive"
+                  size="sm"
+                  className="mt-4"
+                  onClick={() => remove(index)}
+                >
+                  Remove Variant
+                </Button>
+                <Separator className="my-4" />
+              </div>
+            ))}
+
+            <Button
+              type="button"
+              onClick={() =>
+                append({
+                  sku: "",
+                  quantity: 0,
+                  color: "",
+                  size: "",
+                })
+              }
+              className="w-full"
+            >
+              Add New Variant
+            </Button>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </div>
   );
 };
