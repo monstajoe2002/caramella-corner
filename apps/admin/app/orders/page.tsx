d634f9d367bad1071d34aff1f5cb50146d01c7f2
monstajoe2002 <youssef.amr90@gmail.com>
2025-03-07 13:28:30 +0200

    fix: correct page title from "Products" to "Orders"

--- apps/admin/app/orders/page.tsx
+++ apps/admin/app/orders/page.tsx
@@ -4,7 +4,7 @@ import { columns } from "@/components/orders/columns";
 export default function OrdersPage() {
   return (
     <div>
-      <h1>Products</h1>
+      <h1>Orders</h1>
       <div className="mt-10">
         <DataTable columns={columns} data={[]} />
       </div>
