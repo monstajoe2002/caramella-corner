1aee8e1d71c3e3aa653b6b69e65b0711d09f89ad
monstajoe2002 <monstajoe.elshehaby@proton.me>
2025-11-28 16:43:38 +0200

    feat: create navbar component

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,6 +3,7 @@ import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
 
 import appCss from '../styles.css?url'
+import Navbar from '@/components/admin/navbar'
 
 export const Route = createRootRoute({
   head: () => ({
@@ -36,6 +37,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <HeadContent />
       </head>
       <body>
+        <Navbar />
         {children}
         <TanStackDevtools
           config={{
