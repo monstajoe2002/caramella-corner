72411d69562d17f3f43854e29ead59a17a355f85
monstajoe2002 <monstajoe.elshehaby@proton.me>
2025-11-28 21:06:14 +0200

    add category actions

--- src/components/admin/categories/columns.tsx
+++ src/components/admin/categories/columns.tsx
@@ -1,5 +1,16 @@
 import { SelectCatgeory } from '@/db/types'
 import { ColumnDef } from '@tanstack/react-table'
+import { MoreHorizontal, PencilIcon, Trash2Icon } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 export const columns: ColumnDef<SelectCatgeory>[] = [
   {
     accessorKey: 'name',
@@ -11,6 +22,33 @@ export const columns: ColumnDef<SelectCatgeory>[] = [
     cell({ getValue }) {
       const val = getValue() as string[]
       return val.join(', ')
+    },
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      // const category = row.original
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem>
+              <PencilIcon />
+              <span>Edit</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem variant="destructive">
+              <Trash2Icon />
+              <span>Delete</span>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
     },
   },
 ]
