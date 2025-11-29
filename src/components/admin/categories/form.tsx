a1d069c810b7641df65ddbaf031a37234aceeae9
monstajoe2002 <monstajoe.elshehaby@proton.me>
2025-11-29 14:47:42 +0200

    install components for tag input

--- src/components/admin/categories/form.tsx
+++ src/components/admin/categories/form.tsx
@@ -64,6 +64,29 @@ export default function CategoryForm({ data: {} }: CategoryFormProps) {
             )
           }}
         />
+        <form.Field
+          name="subcategories"
+          children={(field) => {
+            const isInvalid =
+              field.state.meta.isTouched && !field.state.meta.isValid
+            return (
+              <Field data-invalid={isInvalid}>
+                <FieldLabel htmlFor={field.name}>Bug Title</FieldLabel>
+                <Input
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                  aria-invalid={isInvalid}
+                  placeholder="Login button not working on mobile"
+                  autoComplete="off"
+                />
+                {isInvalid && <FieldError errors={field.state.meta.errors} />}
+              </Field>
+            )
+          }}
+        />
       </FieldGroup>
     </form>
   )
