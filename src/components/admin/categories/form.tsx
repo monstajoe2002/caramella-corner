7f749c3c37528344b2105105352af73106f3a9da
monstajoe2002 <monstajoe.elshehaby@proton.me>
2025-11-29 14:14:24 +0200

    add name field

--- src/components/admin/categories/form.tsx
+++ src/components/admin/categories/form.tsx
@@ -1,9 +1,10 @@
 import * as z from 'zod'
-import { CategoryWithSubcategories, SelectCategory } from '@/db/types'
+import { CategoryWithSubcategories } from '@/db/types'
+import { useForm } from '@tanstack/react-form'
 const categoryFormSchema = z.object({
   name: z.string().min(1, 'Name is required.'),
   subcategories: z
-    .array(z.string())
+    .array(z.string().min(1))
     .min(1, 'Please enter at least one subcategory.'),
 })
 
@@ -12,5 +13,51 @@ type CategoryFormProps = {
 }
 
 export default function CategoryForm({ data: {} }: CategoryFormProps) {
-  return <div>CategoryForm</div>
+  const form = useForm({
+    defaultValues: {
+      name: '',
+      subcategories: [''],
+    },
+    validators: {
+      onSubmit: categoryFormSchema,
+    },
+    onSubmit: (values) => {
+      console.log(values)
+    },
+  })
+  return (
+    <form
+      id="bug-report-form"
+      onSubmit={(e) => {
+        e.preventDefault()
+        form.handleSubmit()
+      }}
+    >
+      <FieldGroup>
+        <form.Field
+          name="name"
+          children={(field) => {
+            const isInvalid =
+              field.state.meta.isTouched && !field.state.meta.isValid
+            return (
+              <Field data-invalid={isInvalid}>
+                <FieldLabel htmlFor={field.name}>Bug Title</FieldLabel>
+                <Input
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                  aria-invalid={isInvalid}
+                  placeholder="Login button not working on mobile"
+                  autoComplete="off"
+                />
+                {isInvalid && <FieldError errors={field.state.meta.errors} />}
+              </Field>
+            )
+          }}
+        />
+      </FieldGroup>
+    </form>
+  )
 }
