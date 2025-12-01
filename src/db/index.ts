d1c4fbabc57bd88cba4441ddfc14b1f27675ec67
monstajoe2002 <monstajoe.elshehaby@proton.me>
2025-12-01 14:22:07 +0200

    migrate to neon serverless driver

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,10 @@
 import { config } from 'dotenv'
 
-import { drizzle } from 'drizzle-orm/node-postgres'
-import { Pool } from 'pg'
-
+import { drizzle } from 'drizzle-orm/neon-http'
+import { neon } from '@neondatabase/serverless'
 import * as schema from './schema.ts'
 
 config()
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL!,
-})
-export const db = drizzle(pool, { schema })
+const sql = neon(process.env.DATABASE_URL!)
+export const db = drizzle({ client: sql, schema })
