c7c0aba08f05642ee468b63ea44ee4129198b8ba
monstajoe2002 <monstajoe.elshehaby@proton.me>
2025-12-01 14:46:57 +0200

    create an insert type for categories

--- src/db/types.ts
+++ src/db/types.ts
@@ -1,10 +1,14 @@
 import { categories, subcategories } from './schema'
-
+// category
 export type Category = typeof categories.$inferSelect
-export type NewCategory = typeof categories.$inferInsert
+export type NewCategory = Omit<typeof categories.$inferInsert, 'id'>
 
+//subcategory
 export type Subcategory = typeof subcategories.$inferSelect
-export type NewSubcategory = typeof subcategories.$inferInsert
+export type NewSubcategory = Omit<typeof subcategories.$inferInsert, 'id'>
 export type CategoryWithSubcategories = Category & {
   subcategories: Array<Subcategory>
+}
+export type NewCategoryWithSubcategories = NewCategory & {
+  subcategories: Array<NewSubcategory>
 }
