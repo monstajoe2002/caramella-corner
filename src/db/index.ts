788ba33774dad1e6c31a00a5099c8f4ba3ea359f
monstajoe2002 <monstajoe.elshehaby@proton.me>
2025-12-02 19:40:57 +0200

    change driver to node for transaction compatibility

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,12 @@
 import { config } from 'dotenv'
 
-import { drizzle } from 'drizzle-orm/neon-http'
-import { neon } from '@neondatabase/serverless'
+import { drizzle } from 'drizzle-orm/node-postgres'
 import * as schema from './schema.ts'
+import { Pool } from 'pg'
 
 config()
 
-const sql = neon(process.env.DATABASE_URL!)
-export const db = drizzle({ client: sql, schema })
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+})
+export const db = drizzle({ client: pool, schema })
