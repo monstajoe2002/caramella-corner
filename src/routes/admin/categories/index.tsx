7bfa037aef8c5112a3ce9029b1e4d92da3c78e69
monstajoe2002 <monstajoe.elshehaby@proton.me>
2025-12-03 00:19:03 +0200

    display data on the table

--- src/routes/admin/categories/index.tsx
+++ src/routes/admin/categories/index.tsx
@@ -2,27 +2,11 @@ import { columns as categoryCols } from '@/features/admin/categories/components/
 import { DataTable } from '@/components/admin/data-table'
 import { Category } from '@/db/types'
 import { createFileRoute } from '@tanstack/react-router'
-import { createServerFn } from '@tanstack/react-start'
-const getData = createServerFn().handler(async () => {
-  return [
-    {
-      id: '728ed52f',
-      name: 'test',
-      slug: 'test',
-      subcategories: ['a', 'b'],
-    },
-    {
-      id: '728ed52f',
-      name: 'test',
-      slug: 'test',
-      subcategories: ['a', 'b'],
-    },
-    // ...
-  ]
-})
+import { getCategories } from '@/features/admin/categories/data'
+
 export const Route = createFileRoute('/admin/categories/')({
   component: RouteComponent,
-  loader: () => getData(),
+  loader: () => getCategories(),
 })
 
 function RouteComponent() {
