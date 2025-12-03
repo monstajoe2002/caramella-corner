effd6b11b6e5de98e1e1d52fb5a091501cf979f9
monstajoe2002 <monstajoe.elshehaby@proton.me>
2025-12-03 20:05:03 +0200

    display category info

--- src/routes/admin/categories/$id.edit.tsx
+++ src/routes/admin/categories/$id.edit.tsx
@@ -1,10 +1,12 @@
+import { getCategoryById } from '@/features/admin/categories/data'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/admin/categories/$id/edit')({
   component: RouteComponent,
+  loader: async ({ params }) => getCategoryById({ data: params }),
 })
 
 function RouteComponent() {
-  const { id } = Route.useParams()
-  return <div>Hello: {id} </div>
+  const category = Route.useLoaderData()
+  return <div>{JSON.stringify(category, null, 2)} </div>
 }
