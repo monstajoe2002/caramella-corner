2d9792970de1ca00966a33fc90dddac9ba461340
monstajoe2002 <monstajoe.elshehaby@proton.me>
2025-12-05 01:21:29 +0200

    make some fields required

--- src/features/admin/products/components/form.tsx
+++ src/features/admin/products/components/form.tsx
@@ -6,14 +6,14 @@ export const productFormSchema = z.object({
   priceInPiasters: z
     .number('Price must be a number')
     .int()
-    .positive('Price must be 0 or greater'),
+    .positive('Price must be greater than 0'),
   description: z.string().min(1, 'Proper description is required'),
   material: z.string().min(1, 'Material is required'),
   images: z.url('Invalid image URL'), // stored as text, could be JSON string or similar
   categoryId: z.uuid('Category ID must be a valid UUID'),
-  subcategoryId: z.uuid('Subcategory ID must be a valid UUID').optional(),
+  subcategoryId: z.uuid('Subcategory ID must be a valid UUID'),
   active: z.boolean().optional().default(true),
-  quantity: z.number().int().nonnegative().optional().default(0),
+  quantity: z.number().int().nonnegative().default(0),
 })
 type ProductFormProps = {}
 
