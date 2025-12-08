328fb6f228031fb279123b020f96f77f18f48de9
monstajoe2002 <monstajoe.elshehaby@proton.me>
2025-12-08 23:56:58 +0200

    implement insert product function

---
+++ src/features/admin/products/db.ts
@@ -0,0 +1,18 @@
+import { db } from '@/db'
+import { products, variants } from '@/db/schema'
+import { NewProductWithVariants } from '@/db/types'
+
+export async function insertCategory(product: NewProductWithVariants) {
+  return db.transaction(async (trx) => {
+    const [newProduct] = await db.insert(products).values(product).returning()
+    if (product.variants?.length) {
+      const variantValues = product.variants.map((v) => ({
+        ...v,
+        productId: v.productId,
+      }))
+      await trx.insert(variants).values(variantValues)
+    }
+
+    return newProduct
+  })
+}
