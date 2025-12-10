e2e9b2c11e76ca46baa47dd46ba284918d7226c1
monstajoe2002 <monstajoe.elshehaby@proton.me>
2025-12-10 23:45:19 +0200

    call loader

--- src/routes/admin/products/$id.edit.tsx
+++ src/routes/admin/products/$id.edit.tsx
@@ -1,10 +1,14 @@
+import { ProductWithVariants } from '@/db/types'
+import { getProductById } from '@/features/admin/products/data'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/admin/products/$id/edit')({
   component: RouteComponent,
+  loader: async ({ params }) => getProductById({ data: params }),
 })
 
 function RouteComponent() {
+  const data: ProductWithVariants[] = Route.useLoaderData()
   return (
     <div>
       <h1>Edit Category</h1>
