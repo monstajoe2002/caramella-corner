c4bdd358a1d9dd1ab0acc405e3c42f467a5ef642
monstajoe2002 <monstajoe.elshehaby@proton.me>
2025-12-10 23:41:06 +0200

    fetch product from db by ID

--- src/features/admin/products/db.ts
+++ src/features/admin/products/db.ts
@@ -2,10 +2,24 @@ import { db } from '@/db'
 import { products, variants } from '@/db/schema'
 import { NewProductWithVariants } from '@/db/types'
 import { eq } from 'drizzle-orm'
+import { notFound } from '@tanstack/react-router'
 export async function getProductsWithVariants() {
   return await db.query.products.findMany({
     with: { variants: true, category: true },
   })
+}
+export async function getProductById(id: string) {
+  const todo = await db.query.products.findFirst({
+    where: eq(products.id, id),
+    with: {
+      variants: true,
+      category: true,
+      subcategory: true,
+    },
+  })
+
+  if (todo == null) throw notFound()
+  return todo
 }
 export async function insertProduct(product: NewProductWithVariants) {
   return db.transaction(async (trx) => {
