3391fcd668b0610f6ca84d2d1c31c3af87c03c27
monstajoe2002 <monstajoe.elshehaby@proton.me>
2025-12-11 14:09:11 +0200

    implement db function to update product

--- src/db/types.ts
+++ src/db/types.ts
@@ -26,4 +26,6 @@ export type ProductWithVariants = Product & {
 }
 export type NewProductWithVariants = NewProduct & {
   variants: Array<NewVariant>
+  category: Category
+  subcategory: Subcategory
 }
--- src/features/admin/products/db.ts
+++ src/features/admin/products/db.ts
@@ -1,7 +1,7 @@
 import { db } from '@/db'
 import { products, variants } from '@/db/schema'
 import { NewProductWithVariants } from '@/db/types'
-import { eq } from 'drizzle-orm'
+import { and, eq, notInArray } from 'drizzle-orm'
 import { notFound } from '@tanstack/react-router'
 export async function getProductsWithVariants() {
   return await db.query.products.findMany({
@@ -37,4 +37,48 @@ export async function insertProduct(product: NewProductWithVariants) {
 }
 export async function deleteProduct(id: string) {
   return await db.delete(products).where(eq(products.id, id))
+}
+export async function updateProduct(
+  id: string,
+  updatedProduct: NewProductWithVariants,
+) {
+  return db.transaction(async (trx) => {
+    // Update main category fields
+    const [newProduct] = await db
+      .update(products)
+      .set(updatedProduct)
+      .where(eq(products.id, id))
+      .returning()
+
+    if (updatedProduct.variants) {
+      // Extract IDs from updated variants
+      const updatedIds = updatedProduct.variants
+        .map((sc) => sc.id)
+        .filter((id) => id != null)
+
+      // Delete variants not present in updated list
+      await trx
+        .delete(variants)
+        .where(
+          and(eq(variants.productId, id), notInArray(variants.id, updatedIds)),
+        )
+
+      for (const subcat of updatedProduct.variants) {
+        if (subcat.id) {
+          // Update existing variant by id
+          await trx
+            .update(variants)
+            .set(subcat)
+            .where(eq(variants.id, subcat.id))
+        } else {
+          // Insert new variant
+          await trx.insert(variants).values({
+            ...subcat,
+            productId: id,
+          })
+        }
+      }
+    }
+    return newProduct
+  })
 }
