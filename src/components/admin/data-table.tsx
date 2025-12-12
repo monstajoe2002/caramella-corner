42fd92ed219433b4873b13ee6d61226d5fc0fbce
monstajoe2002 <monstajoe.elshehaby@proton.me>
2025-12-12 15:25:56 +0200

    add a search input to data table

--- src/components/admin/data-table.tsx
+++ src/components/admin/data-table.tsx
@@ -1,7 +1,9 @@
 import {
   ColumnDef,
+  ColumnFiltersState,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
   useReactTable,
 } from '@tanstack/react-table'
 
@@ -24,6 +26,8 @@ import {
 import { Button } from '../ui/button'
 import { FileIcon, PlusIcon } from 'lucide-react'
 import { Link, type ToOptions } from '@tanstack/react-router'
+import { useState } from 'react'
+import { Input } from '../ui/input'
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
@@ -36,86 +40,110 @@ export function DataTable<TData, TValue>({
   data,
   newEntryTo,
 }: DataTableProps<TData, TValue>) {
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
   const table = useReactTable({
     data,
     columns,
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
     getCoreRowModel: getCoreRowModel(),
+    state: {
+      columnFilters,
+    },
   })
 
   return (
-    <div className="overflow-hidden rounded-md border mt-8">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext(),
-                        )}
-                  </TableHead>
-                )
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && 'selected'}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
+    <>
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Filter categories..."
+          value={(table.getColumn('name')?.getFilterValue() as string) ?? ''}
+          onChange={(event) =>
+            table.getColumn('name')?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+      </div>
+      <div className="overflow-hidden rounded-md border ">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                    </TableHead>
+                  )
+                })}
               </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                <Empty>
-                  <EmptyHeader>
-                    <EmptyMedia variant="icon">
-                      <FileIcon />
-                    </EmptyMedia>
-                    <EmptyTitle>No categories</EmptyTitle>
-                    <EmptyDescription>
-                      This is where you can add categories to organize your
-                      products. Click <strong>"Create Category"</strong> below
-                      to get started.
-                    </EmptyDescription>
-                  </EmptyHeader>
-                  <EmptyContent>
-                    <Button asChild>
-                      <Link to={newEntryTo}>Create Category</Link>
-                    </Button>
-                  </EmptyContent>
-                </Empty>
-              </TableCell>
-            </TableRow>
-          )}
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && 'selected'}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  <Empty>
+                    <EmptyHeader>
+                      <EmptyMedia variant="icon">
+                        <FileIcon />
+                      </EmptyMedia>
+                      <EmptyTitle>No categories</EmptyTitle>
+                      <EmptyDescription>
+                        This is where you can add categories to organize your
+                        products. Click <strong>"Create Category"</strong> below
+                        to get started.
+                      </EmptyDescription>
+                    </EmptyHeader>
+                    <EmptyContent>
+                      <Button asChild>
+                        <Link to={newEntryTo}>Create Category</Link>
+                      </Button>
+                    </EmptyContent>
+                  </Empty>
+                </TableCell>
+              </TableRow>
+            )}
 
-          {newEntryTo && data.length > 0 ? (
-            <TableRow>
-              <TableCell colSpan={columns.length}>
-                <Button variant={'link'} asChild className="w-full">
-                  <Link to={newEntryTo}>
-                    <PlusIcon />
-                    <span>New</span>
-                  </Link>
-                </Button>
-              </TableCell>
-            </TableRow>
-          ) : null}
-        </TableBody>
-      </Table>
-    </div>
+            {newEntryTo && data.length > 0 ? (
+              <TableRow>
+                <TableCell colSpan={columns.length}>
+                  <Button variant={'link'} asChild className="w-full">
+                    <Link to={newEntryTo}>
+                      <PlusIcon />
+                      <span>New</span>
+                    </Link>
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ) : null}
+          </TableBody>
+        </Table>
+      </div>
+    </>
   )
 }
