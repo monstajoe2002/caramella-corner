a50b7eed40cf92c6d2345bf9fee798358d610f30
monstajoe2002 <monstajoe.elshehaby@proton.me>
2025-12-14 20:50:46 +0200

    enhance OrderItems component to display order details and integrate with order page

--- src/features/admin/orders/components/order-items.tsx
+++ src/features/admin/orders/components/order-items.tsx
@@ -5,5 +5,18 @@ type Props = {
 }
 
 export default function OrderItems({ orderItems }: Props) {
-  return <div>OrderItems</div>
+  return (
+    <div>
+      <h2 className="text-2xl font-bold">Order Items</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {orderItems.map((orderItem) => (
+          <div key={orderItem.id}>
+            <h3 className="text-lg font-bold">
+              {orderItem.quantity} x {orderItem.priceAtOrder}
+            </h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
--- src/routes/admin/orders/$id.tsx
+++ src/routes/admin/orders/$id.tsx
@@ -1,16 +1,21 @@
+import OrderItems from '@/features/admin/orders/components/order-items'
+import { getOrderById } from '@/features/admin/orders/db'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/admin/orders/$id')({
   component: RouteComponent,
+  loader: async ({ params }) => getOrderById(params.id),
 })
 
 function RouteComponent() {
+  const order = Route.useLoaderData()
   return (
     <div>
       <h1 className="text-start">Order Details</h1>
       <p className="text-start text-muted-foreground">
         View the details of the order with the ID: {Route.useParams().id}
       </p>
+      <OrderItems orderItems={order?.orderItems || []} />
     </div>
   )
 }
