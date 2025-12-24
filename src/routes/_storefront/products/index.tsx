a71c30ac9e016e91fc2beaf3f213f43362449cc3
monstajoe2002 <monstajoe.elshehaby@proton.me>
2025-12-24 18:11:25 +0200

    align heading text at the start

--- src/routes/_storefront/products/index.tsx
+++ src/routes/_storefront/products/index.tsx
@@ -11,7 +11,7 @@ function RouteComponent() {
   const products = Route.useLoaderData()
   return (
     <div>
-      <h1>Products</h1>
+      <h1 className="text-start">Products</h1>
       <p>Browse all our exclusive products</p>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center gap-4">
         {products.map((p) => {
