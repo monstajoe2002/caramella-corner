009b835ed43c63f17da5f1fa001db410303c3c70
monstajoe2002 <monstajoe.elshehaby@proton.me>
2025-12-24 18:43:41 +0200

    display product cards

--- src/routes/_storefront/categories/$slug.tsx
+++ src/routes/_storefront/categories/$slug.tsx
@@ -1,9 +1,30 @@
+import ProductCard from '@/components/storefront/products/card'
+import { getProductsByCategorySlug } from '@/features/admin/products/data'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_storefront/categories/$slug')({
   component: RouteComponent,
+  loader: ({ params }) => getProductsByCategorySlug({ data: params }),
 })
 
 function RouteComponent() {
-  return <div>Hello "/_storefront/categories/$slug"!</div>
+  const products = Route.useLoaderData()
+  return (
+    <div>
+      {' '}
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center gap-4">
+        {products.map((p) => {
+          const [thumbnail] = p.images
+          return (
+            <ProductCard
+              key={p.id}
+              {...p}
+              quantity={p.quantity || 0}
+              imageUrl={thumbnail.ikThumbnailUrl}
+            />
+          )
+        })}
+      </div>
+    </div>
+  )
 }
