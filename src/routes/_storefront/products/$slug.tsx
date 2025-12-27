87bcf2b77f5c26fe65a3590497e85c0089521486
monstajoe2002 <monstajoe.elshehaby@proton.me>
2025-12-27 15:30:09 +0200

    display product image crousel

--- src/routes/_storefront/products/$slug.tsx
+++ src/routes/_storefront/products/$slug.tsx
@@ -1,4 +1,13 @@
+import { Card, CardContent } from '@/components/ui/card'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselPrevious,
+  CarouselNext,
+} from '@/components/ui/carousel'
 import { getProductBySlug } from '@/features/admin/products/data'
+import { Image } from '@imagekit/react'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_storefront/products/$slug')({
@@ -11,6 +20,26 @@ function RouteComponent() {
   return (
     <div>
       <h1 className="text-start">{product.name}</h1>
+      <Carousel className="w-full max-w-xs mt-8">
+        <CarouselContent>
+          {product.images.map((img) => (
+            <CarouselItem key={img.id}>
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex aspect-square ">
+                    <Image
+                      src={img.ikUrl}
+                      className="w-full h-full object-cover"
+                    />
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
   )
 }
