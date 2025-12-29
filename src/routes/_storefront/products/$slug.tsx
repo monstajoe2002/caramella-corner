e65f009e36cacbfaf57a7dc15279e3b2ac152da7
monstajoe2002 <monstajoe.elshehaby@proton.me>
2025-12-29 22:58:23 +0200

    display variant dropdown

--- src/routes/_storefront/products/$slug.tsx
+++ src/routes/_storefront/products/$slug.tsx
@@ -11,6 +11,13 @@ import { Image } from '@imagekit/react'
 import { createFileRoute } from '@tanstack/react-router'
 import { Badge } from '@/components/ui/badge'
 import { BadgePercentIcon } from 'lucide-react'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
 
 export const Route = createFileRoute('/_storefront/products/$slug')({
   component: RouteComponent,
@@ -20,54 +27,69 @@ export const Route = createFileRoute('/_storefront/products/$slug')({
 function RouteComponent() {
   const product = Route.useLoaderData()
   return (
-    <div>
-      <div className="md:grid md:grid-cols-2 flex flex-col p-4">
-        <Carousel className="w-full max-w-xs mx-auto">
-          <CarouselContent>
-            {product.images.map((img) => (
-              <CarouselItem key={img.id}>
-                <div className="p-1">
-                  <Card>
-                    <CardContent className="flex aspect-square">
-                      <Image src={img.ikUrl} className="w-full object-cover" />
-                    </CardContent>
-                  </Card>
-                </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          {product.images.length > 1 && (
-            <>
-              <CarouselPrevious />
-              <CarouselNext />
-            </>
-          )}
-        </Carousel>
-        <div className="mt-8 md:mt-0 space-y-2">
-          <h1 className="text-start">{product.name}</h1>
-          <p className="uppercase text-secondary-foreground">
-            {product.category?.name}
-          </p>
-          <Badge>{product.subcategory?.name}</Badge>
-          <h2 className="text-primary">EGP {product.priceAfterDiscount}</h2>
-          {product.discount > 0 && (
-            <h3 className="line-through text-muted-foreground">
-              EGP {product.price}
-            </h3>
-          )}
-          {product.discount > 0 && (
-            <div className="flex gap-1">
-              <BadgePercentIcon />
-              <span className="sr-only">Discount badge</span>
-              <p className="font-semibold mt-0 text-secondary-foreground">
-                {product.discount * 100}% off
-              </p>
-            </div>
-          )}
-          <p>{product.description}</p>
-          <b>Material: </b>
-          <span className="text-muted-foreground">{product.material}</span>
-        </div>
+    <div className="md:grid md:grid-cols-2 flex flex-col p-4">
+      <Carousel className="w-full max-w-xs mx-auto">
+        <CarouselContent>
+          {product.images.map((img) => (
+            <CarouselItem key={img.id}>
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex aspect-square">
+                    <Image src={img.ikUrl} className="w-full object-cover" />
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        {product.images.length > 1 && (
+          <>
+            <CarouselPrevious />
+            <CarouselNext />
+          </>
+        )}
+      </Carousel>
+      <div className="mt-8 md:mt-0 space-y-2">
+        <h1 className="text-start">{product.name}</h1>
+        <p className="uppercase text-secondary-foreground">
+          {product.category?.name}
+        </p>
+        <Badge>{product.subcategory?.name}</Badge>
+        <h2 className="text-primary">EGP {product.priceAfterDiscount}</h2>
+        {product.discount > 0 && (
+          <h3 className="line-through text-muted-foreground">
+            EGP {product.price}
+          </h3>
+        )}
+        {product.discount > 0 && (
+          <div className="flex gap-1">
+            <BadgePercentIcon />
+            <span className="sr-only">Discount badge</span>
+            <p className="font-semibold mt-0 text-secondary-foreground">
+              {product.discount * 100}% off
+            </p>
+          </div>
+        )}
+        <p>{product.description}</p>
+        <b>Material: </b>
+        <span className="text-muted-foreground">{product.material}</span>
+        {/* variant select */}
+        <Select>
+          <SelectTrigger className="w-[300px] mt-4">
+            <SelectValue placeholder="Select a variant..." />
+          </SelectTrigger>
+          <SelectContent>
+            {product.variants.map((variant) => {
+              const { color, size } = variant
+              const variantItem = [color, size].filter(String)
+              return (
+                <SelectItem value={variant.sku}>
+                  {variantItem.join(' - ')}
+                </SelectItem>
+              )
+            })}
+          </SelectContent>
+        </Select>
       </div>
     </div>
   )
