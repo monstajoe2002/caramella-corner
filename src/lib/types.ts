b815d45ff0bc9868537366f972155c98b823caf2
monstajoe2002 <monstajoe.elshehaby@proton.me>
2026-01-05 01:39:36 +0200

    create a client session type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,5 @@
 import { Product, Variant, Image } from '@/db/types'
+import { authClient } from './auth-client'
 
 // cart item type
 export interface CartItem extends Omit<Product, 'price'> {
@@ -6,3 +7,4 @@ export interface CartItem extends Omit<Product, 'price'> {
   image: Image
   price: number
 }
+export type ClientSession = typeof authClient.$Infer.Session
