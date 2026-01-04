0fe850d33680808e02dcb2a81919f6bbb663a7a2
monstajoe2002 <monstajoe.elshehaby@proton.me>
2026-01-05 01:51:47 +0200

    display first letter in the name as a fallback

--- src/components/storefront/user-avatar.tsx
+++ src/components/storefront/user-avatar.tsx
@@ -1,10 +1,13 @@
 import { Avatar, AvatarFallback } from '@/components/ui/avatar'
+import { authClient } from '@/lib/auth-client'
 
-export default function AvatarFallbackDemo() {
+export default function UserAvatar() {
+  const { useSession } = authClient
+  const { data: session } = useSession()
   return (
     <Avatar>
       <AvatarFallback className="bg-indigo-500/25 text-indigo-500">
-        C
+        {session?.user.name[0]}
       </AvatarFallback>
     </Avatar>
   )
