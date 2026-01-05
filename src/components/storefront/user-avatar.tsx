27daabd5a56717af3fc32bdee3d9c5005eda78bc
monstajoe2002 <monstajoe.elshehaby@proton.me>
2026-01-05 02:03:19 +0200

    convert avatar into a dropdown menu with logout item

--- src/components/storefront/user-avatar.tsx
+++ src/components/storefront/user-avatar.tsx
@@ -1,14 +1,37 @@
 import { Avatar, AvatarFallback } from '@/components/ui/avatar'
 import { authClient } from '@/lib/auth-client'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { LogOut } from 'lucide-react'
 
 export default function UserAvatar() {
-  const { useSession } = authClient
+  const { useSession, signOut } = authClient
   const { data: session } = useSession()
   return (
-    <Avatar>
-      <AvatarFallback className="bg-primary/50 text-primary-foreground">
-        {session?.user.name[0]}
-      </AvatarFallback>
-    </Avatar>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarFallback className="bg-primary/50 text-primary-foreground">
+            {session?.user.name[0]}
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>
+          Hello, {session?.user.name.split(' ').shift()}
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={async () => await signOut()}>
+          <LogOut />
+          Sign Out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
