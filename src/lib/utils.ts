c3e2eb9afedbdac4140f8750fb10c24b23d36dd3
monstajoe2002 <monstajoe.elshehaby@proton.me>
2026-01-07 15:51:30 +0200

    generate order number

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,7 +4,17 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
 export function formatVariant(color?: string, size?: string) {
   const variantItem = [color, size].filter(String)
   return variantItem.join(' - ')
+}
+
+export function generateOrderNumber(length = 8): string {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+  let result = 'ORD-'
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length))
+  }
+  return result
 }
