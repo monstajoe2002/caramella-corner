6a215dc9322d563a2fe704b1a3959e328222e234
monstajoe2002 <monstajoe.elshehaby@proton.me>
2026-01-07 16:01:31 +0200

    implement insert order function

--- src/features/storefront/orders/db.ts
+++ src/features/storefront/orders/db.ts
@@ -1,5 +1,23 @@
 import { db } from '@/db'
-import { orders } from '@/db/schema'
-import { NewOrder } from '@/db/types'
+import { orderItems, orders } from '@/db/schema'
+import { NewOrderWithItems } from '@/db/types'
+import { generateOrderNumber } from '@/lib/utils'
 
-export async function insertOrder(order: NewOrder) {}
+export async function insertOrder(order: NewOrderWithItems) {
+  return db.transaction(async (trx) => {
+    const [newOrder] = await db
+      .insert(orders)
+      .values({
+        ...order,
+        status: 'pending',
+        orderNumber: generateOrderNumber(),
+      })
+      .returning()
+    if (order.orderItems?.length) {
+      const orderItemsValues = order.orderItems
+      await trx.insert(orderItems).values(orderItemsValues)
+    }
+
+    return newOrder
+  })
+}
